8c16152b5d2439745691c13c808981b599dd1a74
Prateek Mishra <pikuaja@gmail.com>
2023-10-21 22:54:47 -0500

    cwi calc

---
+++ merger.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# Load your event dataset into a DataFrame (replace 'event_data.csv' with your file path)
+event_df = pd.read_csv("datasets/final_data/complete_event_list.csv")
+
+# Load your water quality dataset into another DataFrame (replace 'water_quality_data.csv' with your file path)
+water_quality_df = pd.read_csv("aggregated_data.csv")
+
+# Assuming both datasets have a 'date' column, convert it to a datetime format
+event_df["Date"] = pd.to_datetime(event_df["Date"])
+water_quality_df["Date"] = pd.to_datetime(water_quality_df["Date"])
+
+# Merge the two DataFrames based on the 'date' column
+merged_df = pd.merge(event_df, water_quality_df, on="Date", how="inner")
